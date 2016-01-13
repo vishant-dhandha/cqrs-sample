20a0f2472366f44518f5dd122e20f20f9b6e9835
Vishant <vishant.dhandha@bizos.net>
2016-01-13 12:11:25 +0530

    added custom function to append some info to event

--- domain/lib/item/createItem.js
+++ domain/lib/item/createItem.js
@@ -1,5 +1,28 @@
+var http = require('http');
+function fetchURL(url, done) {
+    var body = '';
+    http.get(url, function(res){
+        res.on('data', function(chunk){
+            body += chunk.toString();
+        });
+
+        res.on('end', function(chunk){
+            done(null, body);
+        });
+    }).on('error', function(e){
+            done(e);
+        });
+}
+var append = function(data,callback){
+    fetchURL('http://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA', function(err,res){
+        data.info1 = res
+        callback(null)
+    });
+}
 module.exports = require('cqrs-domain').defineCommand({
   name: 'createItem'
 }, function (data, aggregate) {
-  aggregate.apply('itemCreated', data);
+    append(data,function(err,res){
+        aggregate.apply('itemCreated', data);
+    })
 });
